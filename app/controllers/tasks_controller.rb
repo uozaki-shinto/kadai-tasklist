d1971a9e197600fc413ee86a296747724d308de5
shinto.uozaki <uozaki.shinto@imagicalab.co.jp>
2019-06-07 09:14:14 +0000

    bug fixed3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:show,:edit,:update,:destroy]
-  before_action :require_user_logged_in
+  
   
     def index
        @tasks = current_user.tasks.all.page(params[:page])
