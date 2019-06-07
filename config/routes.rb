d71887d4175645d5697e983a8aa949a017108cab
shinto.uozaki <uozaki.shinto@imagicalab.co.jp>
2019-06-07 08:09:07 +0000

    code organization

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'toppages/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
-  
-  resources :tasks
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
   
   resources :tasks, only: [:create, :destroy,:edit]
 end
